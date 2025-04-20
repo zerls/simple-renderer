713a91f0bc5be0b38c46ea23c06462fa20dbcfb8
夏酱 <18399670+zerls@users.noreply.github.com>
2025-04-20 14:47:29 +0800

    Refactor code

--- test.h
+++ test.h
@@ -6,5 +6,5 @@ std::shared_ptr<Mesh> createTriangleMesh(const Color &color);
 
 // 将帧缓冲区保存为 PPM 图像文件
 void saveToPPM(const std::string& filename, const FrameBuffer& frameBuffer);
-   
+// 将深度缓冲区保存为 PPM 图像文件
 void saveDepthMap(const std::string &filename, const FrameBuffer& frameBuffer,float nearPlane, float farPlane);
